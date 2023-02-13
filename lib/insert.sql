607c7ed0dbd5d93db0efb681aa588f779de965ee
fusion407 <fusion407@gmail.com>
2023-02-12 21:50:42 -0600

    commit tree 20f553c2f71db1f1c44a2a10dd1d63f37bb895b4

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr, Chocolate', 42, 'M', 'Brown', 'Wild', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 26, 'M', 'Brown', 'Wild', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 48, 'F', 'Brown', 'Wild', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 53, 'M', 'Brown', 'Wild', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 49, 'F', 'Brown', 'Wild', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 35, 'M', 'Brown', 'Wild', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 16, 'F', 'Brown', 'Wild', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 42, 'M', 'Brown', 'Wild', 1);
